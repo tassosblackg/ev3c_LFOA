0e1e7f06561495bdbb31f4c52fa1937ff9d39a33
soylisK <soylis.k@gmail.com>
2017-12-02 15:22:42 +0200

    add some doc's and comments for demoR

--- demoRuns/testR.c
+++ demoRuns/testR.c
@@ -1,3 +1,8 @@
+/***testR.c is a demo test for checking dc-motors functionality
+**for more infos about registers' data and values check
+**hitechnic controller's manual
+**/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,21 +15,22 @@ int main()
 {
 
         printf("Main runsssss....\n");
-        int8_t fd = open("/dev/i2c-6",O_RDWR);
-        printf("FILE discriptor : fd=%d\n",fd);
+	//i2c-6 =i2c-(port_number+2)...in this case we have input4 ,so 4+2=6..changes depends on how you connected your controler
+        int8_t fd = open("/dev/i2c-6",O_RDWR); //open device file ..(hitechnic controller connected to input4 on ev3 brick)
+        printf("FILE discriptor : fd=%d\n",fd); 
         if(fd>0)
         {
                 ioctl(fd,I2C_SLAVE,0x01);
-                unsigned char buf[5]={0x44,0x00,0x00,0x00,0x00};
+                unsigned char buf[5]={0x44,0x00,0x00,0x00,0x00}; //initialize controller [register's address,motor1's mode,motor1's_pwm,motor2's_pwm,motor2's_pwm]
               	int w1= write(fd,buf,5);
 		 // int w1=i2c_smbus_write_block_data(fd,0x45,1,buf);
-                printf("Init write...w1=%d\n",w1);
+                printf("Init write...w1=%d\n",w1); //check if write accomplished
 		//close(fd);
                 
 		
-                unsigned char buf2[3]={0x45,0x9,-0x9};
+                unsigned char buf2[3]={0x45,0x9,-0x9}; //moving backwards.. [register's address,motor1's_pwm,motor2's_pwm]
                 int w2=write(fd,buf2,3);
-                printf("FIRST MOTOR write...w2=%d\n",w2);
+                printf("FIRST MOTOR write...w2=%d\n",w2); //check if write accomplished
                /* unsigned char buf3[2]={0x46,-0x20};
                 int w3=write(fd,buf3,2);
                 printf("SECOND MOTOR write..w3=%d\n",w3);*/
--- demoRuns/testS.c
+++ demoRuns/testS.c
@@ -1,3 +1,6 @@
+/**demoS.c is a test for servo motor functionallity
+**for more infos check hitechnic controller's manual-doc\
+**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -12,20 +15,20 @@ int main()
         printf("FILE discriptor : fd=%d\n",fd);
         if(fd>0)
         {
-                ioctl(fd,I2C_SLAVE,0x01);
-                unsigned char buf[2]={0x48,0xAA};
+                ioctl(fd,I2C_SLAVE,0x02);
+                unsigned char buf[2]={0x48,0xAA}; //initiallize servo mode
                 int w1= write(fd,buf,2);
                 printf("Init write...w1=%d\n");
-                unsigned char readb[200];
+                /*unsigned char readb[200];
 		read(fd,readb,200);
 		int i;
 		for(i=0;i<200;i++)
-			printf("read[%d]=%c\n",i,readb[i]);
+			printf("read[%d]=%c\n",i,readb[i]);*/ //read data to see if everything is fine
 		//close(fd);
 		printf("******\n");
-		unsigned char buf2[2]={0x42,0x37};
+		unsigned char buf2[2]={0x42,0x37}; //set value(0x37) to servo at channel 1 ->0x42
 		int w2=write(fd,buf2,2);
-                printf("w2=%d\n",w2);
+                printf("w2=%d\n",w2); //checks if write accomplished
                 
         }
 
